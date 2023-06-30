ace94077542a587d73f4f252b3f68f5c320e5046
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-06-29 20:23:17 -0400

    Add files via upload

---
+++ dating_interpreter.rb
@@ -0,0 +1,5 @@
+require "parslet"
+
+require_relative "_parsers/ahusacosdata.rb"
+
+AhusacosLang::AhusacosCMD.dating_outcomes
---
+++ fate_interpreter.rb
@@ -0,0 +1,5 @@
+require "parslet"
+
+require_relative "_parsers/ahusacosdata.rb"
+
+AhusacosLang::AhusacosCMD.character_fates
